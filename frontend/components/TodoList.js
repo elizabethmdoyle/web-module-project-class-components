531f6da77b7afb808c98a55aebf91cca4f5297f4
Elizabeth Doyle <myoddjourney@gmail.com>
2023-09-22 15:33:52 -0400

    finished project

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -21,21 +21,32 @@ import Todo from "./Todo"
 
 
 export default class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showAll: true
+    }
+  }
 
+  toggleCompleted = (evt) => {
+    this.setState({...this.state, showAll: !this.state.showAll})
+  }
 
   render() {
     //destructured todo from the props object - props does not need super to be called in a React.component
     //cannot add this.props in the return statement, as only jsx elements can be renedred in the return statement
     const { todos, toggleTodo } = this.props;
-    console.log(todos)
+    console.log(this.props)
+    const filtered = todos.filter(td => {!td.completed || this.state.showAll})
+    console.log(filtered)
     return (
 
 
       <div>
-        {todos.map(item => {
-          return <Todo key={todos.id} todo={item} toggleTodo={toggleTodo}/>
+        {filtered.map(item => {
+          return <Todo key={item.id} todo={item} toggleTodo={toggleTodo}/>
         })}
-       <button clearCompleted={this.clearCompleted} >Clear Completed</button>
+       <button onClick={this.toggleCompleted} >Show All</button>
 
 
       </div>
